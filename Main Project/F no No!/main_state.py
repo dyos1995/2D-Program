b50ed72784275621f76cc693927f4e10b2eb024c
O sung <dyos1995@daum.net>
2017-11-07 23:55:20 +0900

    main
    main project

--- Main Project/F no No!/main_state.py
+++ Main Project/F no No!/main_state.py
@@ -15,7 +15,7 @@ boy = None
 grass = None
 font = None
 
-
+running = None
 
 class Grass:
     def __init__(self):
@@ -27,30 +27,54 @@ class Grass:
 
 
 class Boy:
+    image = None
+
+    LEFT_RUN, RIGHT_RUN, LEFT_STAND, RIGHT_STAND = 0, 1, 2, 3
+
+
     def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
-        self.image = load_image('run_animation.png')
-        self.dir = 1
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.state = self.RIGHT_STAND
+        if Boy.image == None:
+            Boy.image = load_image('run_animation.png')
+
+    def handle_event(self, event):
+        if(event.type, event.key) == (SDL_KEYDOWN, SDLK_LEFT):
+            if self.state in (self.RIGHT_STAND, self.LEFT_STAND):
+                self.state = self.LEFT_RUN
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_RIGHT):
+            if self.state in (self.RIGHT_STAND, self.LEFT_STAND):
+                self.state = self.RIGHT_RUN
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_LEFT):
+            if self.state in (self.LEFT_RUN,):
+                self.state = self.LEFT_STAND
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_RIGHT):
+            if self.state in (self.RIGHT_RUN,):
+                self.state = self.RIGHT_STAND
+
 
     def update(self):
-        self.frame = (self.frame + 1) % 8
-        self.x += self.dir
-        if self.x >= 800:
-            self.dir = -1
-        elif self.x <= 0:
-            self.dir = 1
+        self.frame = (self.frame + 1) % 6
+        if self.state == self.RIGHT_RUN:
+            self.x = min(800, self.x + 5)
+        elif self.state == self.LEFT_RUN:
+            self.x = max(0, self.x - 5)
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 60, self.state * 80, 60, 80, self.x, self.y)
 
 
 def enter():
-    pass
+    global boy, grass
+    boy = Boy()
+    grass = Grass()
 
 
 def exit():
-    pass
+    global boy, grass
+    del(boy)
+    del(grass)
 
 
 def pause():
@@ -62,15 +86,26 @@ def resume():
 
 
 def handle_events():
-    pass
+    global boy
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        else:
+            boy.handle_event(event)
 
 
 def update():
-    pass
+   boy.update()
 
 
 def draw():
-    pass
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
 
 
 

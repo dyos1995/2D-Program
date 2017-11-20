0f695a3f5445dc253e90f80ca50cb5bcca929b22
O sung <dyos1995@daum.net>
2017-11-20 20:04:58 +0900

    2017182004
    151515

--- Main Project/F no No!/mygame.py
+++
@@ -1,9 +0,0 @@
-import game_framework
-import start_state
-from pico2d  import *
-import main_state
-
-game_framework.run(start_state)
-#open_canvas()
-#game_framework.run(main_state)
-#close_canvas()
